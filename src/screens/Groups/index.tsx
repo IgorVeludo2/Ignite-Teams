f5fd88a8ba45096d8796b2203b719948760882ca
IgorVeludo2 <velvet.guimaraes@gmail.com>
2022-10-19 13:01:36 -0300

    groupCard component

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,6 +1,8 @@
-import { Container } from './styles'
 import { Highlight } from '@components/Highlight';
 import { Header } from '@components/Header';
+import { GroupCard } from '@components/GroupCard';
+
+import { Container } from './styles'
 
 export function Groups() {
   return (
@@ -10,6 +12,8 @@ export function Groups() {
         title='Turmas' 
         subtitle='jogue com suas turmas'
       />
+
+      <GroupCard title="Galera do Ignite"/>
     </Container>
   );
 }
