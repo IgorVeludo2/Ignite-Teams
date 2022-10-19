62e6aab18712aa4f7ab8238f11c1b03566ffff4c
IgorVeludo2 <velvet.guimaraes@gmail.com>
2022-10-19 11:18:08 -0300

    Highlight component

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,10 +1,15 @@
 import { Container } from './styles'
+import { Highlight } from '@components/Highlight';
 import { Header } from '@components/Header';
 
 export function Groups() {
   return (
     <Container>
       <Header />
+      <Highlight 
+        title='Turmas' 
+        subtitle='jogue com suas turmas'
+      />
     </Container>
   );
 }
