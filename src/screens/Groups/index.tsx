4cb8a35ebee9cb02a37cd0adb93e9829361f5dad
IgorVeludo2 <velvet.guimaraes@gmail.com>
2022-10-23 11:37:00 -0300

    group flatList

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,3 +1,6 @@
+import { useState } from 'react';
+import { FlatList } from 'react-native';
+
 import { Highlight } from '@components/Highlight';
 import { Header } from '@components/Header';
 import { GroupCard } from '@components/GroupCard';
@@ -5,15 +8,25 @@ import { GroupCard } from '@components/GroupCard';
 import { Container } from './styles'
 
 export function Groups() {
+  const [groups, setGroups] = useState<string[]>(['Galera da Rocket']);
+
   return (
     <Container>
       <Header />
       <Highlight 
         title='Turmas' 
-        subtitle='jogue com suas turmas'
+        subtitle='jogue com suas turma'
+      />
+      <FlatList 
+        data={groups}
+        keyExtractor={item => item}
+        renderItem={({ item }) =>  (
+          <GroupCard 
+            title={item}
+          />
+        )}
       />
 
-      <GroupCard title="Galera do Ignite"/>
     </Container>
   );
 }
