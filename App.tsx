f935fe4d286fa43ee4992078b89023b41dbc4e06
IgorVeludo2 <velvet.guimaraes@gmail.com>
2022-10-31 16:13:14 -0300

    newGroup interface

--- App.tsx
+++ App.tsx
@@ -7,6 +7,7 @@ import { Loading } from '@components/Loading';
 import theme from './src/theme';
 
 import { Groups } from '@screens/Groups';
+import { NewGroup } from '@screens/NewGroups';
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -21,7 +22,7 @@ export default function App() {
         backgroundColor="transparent"
         translucent
       />
-      { fontsLoaded ? <Groups /> : <Loading /> }
+      { fontsLoaded ? <NewGroup /> : <Loading /> }
     </ThemeProvider>
   );
 }
